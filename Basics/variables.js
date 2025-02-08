a5319fdbfd5b81c11c7e896a6605deaa61a30470
u4-Ujjawal <ujjwalchoudhary12345.uc82@gmail.com>
2025-02-08 15:43:48 +0530

    vaiables

---
+++ Basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345;
+let accountEmail = "xyz@gmail.com";
+var accountPass = "12345";
+accountCity = "Jaipur";
+
+// accountId = 123;// not allowed
+console.log("accountId");
+
+accountEmail = "abc";
+accountPass = "1212";
+accountCity = "Indore";
+
+console.table([accountEmail, accountPass, accountCity]);
