bae5244ff5f65c3995f684141ac7544f67dc42ef
u4-Ujjawal <ujjwalchoudhary12345.uc82@gmail.com>
2025-02-14 11:13:03 +0530

    datatypes

---
+++ Basics/datatype.js
@@ -0,0 +1,10 @@
+let age = 18;
+let name = "Ujjawal";
+let pass = null; //null is defined as object in js
+
+let state; //state is undefined
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof pass);
+console.log(typeof state);
