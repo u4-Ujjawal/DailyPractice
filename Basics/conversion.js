1faaa0b53cd34d272259cf4ccaf9d8e0c1536968
u4-Ujjawal <ujjwalchoudhary12345.uc82@gmail.com>
2025-02-14 11:50:53 +0530

    conversion

---
+++ Basics/conversion.js
@@ -0,0 +1,18 @@
+/*number to string
+let scoreofGame = 33;
+console.log(typeof scoreofGame);
+let stringscoreofGame = String(scoreofGame); 
+console.log(typeof stringscoreofGame);*/
+
+/*let win = true;
+console.log(typeof win);
+let Numberwin = Number(win);
+console.log(typeof Numberwin);
+console.log(Numberwin);*/
+
+//let win = ""; false output
+let win = "ujjawal"; //bollean output will be true
+console.log(typeof win);
+let Numberwin = Boolean(win);
+console.log(typeof Numberwin);
+console.log(Numberwin);
