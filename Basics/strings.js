22c01e2b41bb45aba362b81f17cd568828e9b9f9
u4-Ujjawal <ujjwalchoudhary12345.uc82@gmail.com>
2025-02-15 16:29:49 +0530

    String

---
+++ Basics/strings.js
@@ -0,0 +1,10 @@
+let s1 = "Ujjawal";
+const s2 = "Choudhary";
+let age = 20;
+console.log(`My name is ${s1} ${s2} and I am ${age} years old.`);
+
+console.log(`My name is of ${s1.length} letters `);
+
+console.log(`My name is of ${s1.toUpperCase()} letters `);
+
+// console.log(`My name is of ${s1.split()} letters `);
